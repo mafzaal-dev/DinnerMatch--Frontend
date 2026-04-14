7bed0815047da9a64239567ceb5af01745344f67
afzaal <m.afzaal@sparkai.ca>
2026-04-14 20:04:57 +0500

    → feat(constants/preferences): add language, budget, and menu choice constants for user preferences

---
+++ src/constants/preferences.js
@@ -0,0 +1,3 @@
+export const LANGUAGES = ['English', 'Afrikaans', 'Xhosa', 'Zulu'];
+export const BUDGETS = ['$', '$$', '$$$'];
+export const MENU_CHOICES = ['Vegetarian', 'Meat', 'Fish', 'Vegan', 'Halaal'];
